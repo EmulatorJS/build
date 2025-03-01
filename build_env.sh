729382d040d62412fc7f609d6fba9e6e47aaae9e
Michael Green <84688932+michael-j-green@users.noreply.github.com>
2025-03-01 15:19:08 +1100

    Build a nightly and release action for building cores (#18)
    * Baseline build script and build environment
    
    * action-test
    
    * remove sudo
    
    * Modify apt package
    
    * Switch to distro package of emscripten
    
    * WIP
    
    * WIP
    
    * Fix zip path
    
    * More tweaks
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Restore cores.json
    
    * WIP
    
    * Lock emsdk version
    
    * Restore cores.json
    
    * Upload to CDN
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Revert "WIP"
    
    This reverts commit c7f7f8c4955c0b66db600e0d6d86c080cb32889b.
    
    * Implemented requested changes

--- build_env.sh
+++ build_env.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 cd /workspaces/build
-git clone https://github.com/emscripten-core/emsdk.git .emsdk
-cd .emsdk
-git checkout 3.1.54
-./emsdk install 3.1.54
-./emsdk activate 3.1.54
+rm -fR emsdk
+git clone https://github.com/emscripten-core/emsdk.git
+cd emsdk
+./emsdk install 3.1.74
+./emsdk activate 3.1.74
 source ./emsdk_env.sh
-wget https://raw.githubusercontent.com/EmulatorJS/build/main/emscripten.patch
-patch -u -p0 -i ../emscripten.patch
-cd ../
+cd ..
